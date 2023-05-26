406a7e6b328ec1437a71f1055bf9f002fc4d05ab
alexbaezah <alcebaezah@gmail.com>
2023-05-26 14:45:28 -0400

    ahora el create de webpay toma como argumento el pago

--- api/api.py
+++ api/api.py
@@ -1,16 +1,18 @@
 import random
 from flask import jsonify, request, Blueprint
 from transbank.webpay.webpay_plus.transaction import Transaction
+from model import Pago, db, Carrito
 
 
 webpay_blueprint = Blueprint('webpay_plus', __name__)
 
-@webpay_blueprint.route("/create", methods=["GET"])
-def webpay_plus_create():
+@webpay_blueprint.route("/create/<int:id_pago>", methods=["GET"])
+def webpay_plus_create(id_pago):
     print("Webpay Plus Transaction.create")
-    buy_order = str(random.randrange(1000000, 99999999))
+    pago = Pago.query.get(id_pago)
+    buy_order = str(pago.ID_PAGO)
     session_id = str(random.randrange(1000000, 99999999))
-    amount = random.randrange(10000, 1000000)
+    amount = str(pago.MONTO_TOTAL_PAGO)
     return_url = request.url_root + 'webpay-plus/commit'
 
     create_request = {
@@ -28,3 +30,23 @@ def webpay_plus_create():
         "request": create_request,
         "response": response
     })
+
+
+
+
+@webpay_blueprint.route("commit", methods=["GET"])
+def webpay_plus_commit():
+    token = request.args.get("token_ws")
+    print("commit for token_ws: {}".format(token))
+
+    response = (Transaction()).commit(token=token)
+    print("response: {}".format(response))
+
+    # Construye la respuesta en formato JSON
+    response_json = {
+        "token": token,
+        "response": response
+    }
+
+    # Retorna la respuesta en formato JSON
+    return jsonify(response_json)
